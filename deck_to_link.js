71f41c410e9c2f9a32f31eca120f10ff9d91016f
aguscerdo <agus.marinovic@gmail.com>
2019-05-29 22:25:25 -0700

    deck_to_link

--- deck_to_link.js
+++ deck_to_link.js
@@ -1,32 +1,57 @@
-function deck_to_link() {
-	const code = "ab23";
-	const user = "aguscerdo";
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-	if (! code) {
-		return 'No deck code';
-	} else {
-		const http = new XMLHttpRequest();
-		const link = "https://shadowverse-portal.com/api/v1/deck/import?format=json&deck_code=" + str(code) +"&lang=en";
-		http.open('GET', link, false);
-		http.send();
+function process_request(req) {
+	var user = req.query.user;
+	var code = req.query.code;
 
-		http.onreadystatechange = function(e) {
-			if  (http.readyState == 4) {
-				if (http.status == 200) {
-					var req = JSON.parse(http.responseText);
-
-					var clan = req.data.clan;
-
-					if (!clan || clan === null || clan == 'null') {	
-						return 'Invalid deck code';
-					} else {	
-						var hash = req.data.hash;	
-						return str(user) + '\'s deck --> https://shadowverse-portal.com/deck/'+ hash +'?lang=en';
-					}
-				} else {
-					return "An error ocurred... Try again"
-				}
-			}
+	return new Promise(resolve => {
+		if (!code) {
+			resolve("No deck code passed")
 		}
+		else {
+			var xhr = new XMLHttpRequest();
+			const link = "https://shadowverse-portal.com/api/v1/deck/import?format=json&deck_code=" + String(code) +"&lang=en";
+			xhr.open('GET', link, true);
+
+			xhr.onreadystatechange = function() {
+				if  (xhr.readyState == 4) {
+					if (xhr.status == 200) {
+						var req = JSON.parse(xhr.responseText);
+						console.log(req.data)
+						var clan = req.data.clan;
+
+						if (!clan || clan === null || clan == 'null') {
+							resolve('Invalid deck code');
+						} else {
+							var hash = req.data.hash;
+							resolve(String(user) + '\'s deck --> https://shadowverse-portal.com/deck/'+ hash +'?lang=en');
+						}
+					} else {
+						resolve("An error ocurred... Try again");
+					}
+				}
+			};
+			xhr.send();
+		}
+	});
+}
+
+function deck_to_link(code, user) {
+	if (! code) {
+		return new Promise(function(resolve, reject) {
+			resolve('No deck code');
+		});
+	} else {
+		var xhr = new XMLHttpRequest();
+		const link = "https://shadowverse-portal.com/api/v1/deck/import?format=json&deck_code=" + String(code) +"&lang=en";
+		xhr.open('GET', link, false);
+
+		return new Promise(function (resolve, reject) {
+
+
+		});
 	}
 }
+
+module.exports.deck_to_link = deck_to_link
+module.exports.process_request = process_request
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const sv_handler = require('./deck_to_link.js');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const PORT = process.env.PORT || 5000;
+
+app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/sv_deck', (req, res) =>
+    sv_handler.process_request(req)
+    .then(function(ret) {
+        res.send(ret)}));
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
